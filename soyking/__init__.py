9419f10e97630bc575102dda74f80c08544dd3c5
Andrea Rujano <andrea.jenir@mipwise.com>
2022-05-13 09:55:15 -0300

    Added configuration files and action engine code to update shipping costs

--- soyking/__init__.py
+++ soyking/__init__.py
@@ -1,6 +1,12 @@
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from soyking.schemas import input_schema, output_schema
 from soyking.action_update_shipping_cost import update_shipping_cost
 from soyking.main import solve
 
+actions_config = {
+    'Update Shipping Cost': {
+        'schema': 'input',
+        'engine': update_shipping_cost,
+        'tooltip': "Update the shipping cost by the factor entered in the 'Shipping Cost Multiplier' parameter"}
+    }
