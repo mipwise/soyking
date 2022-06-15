b247bf4546ad090a00584882d7612950f566a384
Andrea Jeniree Rujano <45374816+andreajrujano@users.noreply.github.com>
2022-06-15 14:21:40 -0300

    Update action_update_shipping_cost.py

--- soyking/action_update_shipping_cost.py
+++ soyking/action_update_shipping_cost.py
@@ -4,7 +4,7 @@ from soyking import input_schema
 def update_shipping_cost(dat):
     shipping_costs = dat.shipping_costs.copy()
     params = input_schema.create_full_parameters_dict(dat)
-    shipping_costs['Cost per ton'] = params['Shipping Cost Multiplier'] * shipping_costs['Cost per ton']
-    shipping_costs = shipping_costs.round({'Cost per ton': 1})
+    shipping_costs['Cost Per Ton'] = params['Shipping Cost Multiplier'] * shipping_costs['Cost Per Ton']
+    shipping_costs = shipping_costs.round({'Cost Per Ton': 1})
     dat.shipping_costs = shipping_costs
     return dat
