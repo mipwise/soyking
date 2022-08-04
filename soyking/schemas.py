c01cccbd5bfadde9a7c92a4868ae01baf25d18f5
Andrea Rujano <andrea.jenir@mipwise.com>
2022-08-04 12:27:09 -0300

    Added new parameters to schemas

--- soyking/schemas.py
+++ soyking/schemas.py
@@ -16,6 +16,10 @@ input_schema = PanDatFactory(
 # region USER PARAMETERS
 input_schema.add_parameter('Shipping Cost Multiplier', default_value=1.5, number_allowed=True, strings_allowed=(),
                            must_be_int=False, min=0.0, inclusive_min=True, max=10, inclusive_max=True)
+input_schema.add_parameter('Time Limit', default_value=120, number_allowed=True, strings_allowed=(),
+                           must_be_int=False, min=0.0, inclusive_min=True, max=1*60**2, inclusive_max=True)
+input_schema.add_parameter('MIP Gap', default_value=0.01, number_allowed=True, strings_allowed=(),
+                           min=0, inclusive_min=False, max=1, inclusive_max=False)
 # endregion
 
 # region OUTPUT SCHEMA
@@ -40,7 +44,7 @@ input_schema.set_data_type(table=table, field='Demand', number_allowed=True, str
 table = 'shipping_costs'
 for field in ['Farm ID', 'DC ID']:
     input_schema.set_data_type(table=table, field=field, number_allowed=False, strings_allowed='*')
-input_schema.set_data_type(table=table, field='Cost per ton', number_allowed=True, strings_allowed=(),
+input_schema.set_data_type(table=table, field='Cost Per Ton', number_allowed=True, strings_allowed=(),
                            min=0, inclusive_min=True, max=float('inf'), inclusive_max=False)
 input_schema.add_foreign_key(native_table='shipping_costs', foreign_table='suppliers',
                              mappings=('Farm ID', 'Farm ID'))
